a64e7839534c2a0ee54305f52713e6ee6929e5fd
Daniel Brior <djbrior@gmail.com>
2025-01-12 22:30:23 -0800

    Initial Commit

---
+++ Locked In/Locked_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Locked_InApp.swift
+//  Locked In
+//
+//  Created by Daniel Brior on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Locked_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
